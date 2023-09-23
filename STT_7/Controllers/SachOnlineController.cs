c9e001c678bed96e32a40f9b4c236c42ce7eb276
thanh9y <145832602+thanh9y@users.noreply.github.com>
2023-09-23 15:59:48 +0700

    1

---
+++ STT_7/Controllers/SachOnlineController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace STT_7.Controllers
+{
+    public class SachOnlineController : Controller
+    {
+        // GET: SachOnline
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ChuDePartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult NhaXuatBanPartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult NavPartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult FooterPartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult SachBanNhieuPartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult SliderPartial()
+        {
+            return PartialView();
+        }
+
+    }
+}
